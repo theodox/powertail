da50c93c93c15c973e916a57ef9d090103d7da99
Steve Theodore <steve@theodox.com>
2015-05-07 09:08:26 -0700

    initial

---
+++ power/__init__.py
@@ -0,0 +1,32 @@
+__author__ = 'stevet'
+
+from Threading inport Lock
+
+
+class PowerTail(object):
+
+
+    def __init__(self):
+
+        self._state = False
+        self._internal_state = self._state
+        self.lock = Lock(False)
+
+    def state(self):
+        with self.lock:
+            return _state
+
+    def on(self):
+        with self.lock:
+            self._state = True
+            self.enact()
+
+    def off(self):
+        with self.lock:
+            self.state = False
+            self.enact()
+
+    def enact(self):
+        if self._state != self._internal_state:
+            print "changing to", self._state
+            self._internal_state = self._state
